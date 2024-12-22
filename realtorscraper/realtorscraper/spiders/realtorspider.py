2e77bc027b9efe4b65c29197e367da8aec0f4013
luldeezy <leeks.shaken11@icloud.com>
2024-12-22 16:04:20 -0600

    Update
    Range description for pages
    Coords added
    Shows Full url
    City/State added
    Removed Primary Photo for now
    
    More future updates will include
    - All photos available
    -  overall more information avaliable

--- realtorscraper/realtorscraper/spiders/realtorspider.py
+++ realtorscraper/realtorscraper/spiders/realtorspider.py
@@ -1,15 +1,16 @@
 import scrapy
 import json
 
-    # Data being pulled 
+    # Data being pulled from site, pages in range 1-17, this can be changed to numbers avaliable
 class RealtorSpider(scrapy.Spider):
     name = "realtorspider"
-    start_urls = ["https://www.realtor.com/realestateandhomes-search/Madison_WI"]
+    start_urls = [f"https://www.realtor.com/realestateandhomes-search/Madison_WI/pg-{page}"
+    for page in range (1, 17)
 
+]   # Taking the embedded JSON from the webpage
     def parse(self, response):
-        # Taking the embedded JSON from the webpage
         next_data_script = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
-        # will show error if script does not work
+        # will show error if pulling json does not work
         if not next_data_script:
             self.logger.error("Failed to find __NEXT_DATA__ script on the page")
             return
@@ -26,26 +27,18 @@ class RealtorSpider(scrapy.Spider):
 
         for home in homes:
             home_data = {
-                "home type": home["description"].get("type", None),
-                "posted": home.get("list_date", None),
-                "home URL": home.get("permalink", None),
-                "home main image": home["primary_photo"].get("href", None),
-                "home status": home.get("status", None),
-                "home price": home.get("list_price", None),
-                "home address": home["location"]["address"].get("line", None),
-                "home zipcode": home["location"]["address"].get("postal_code", None),
-                "num beds": home["description"].get("beds", None),
-                "num baths": home["description"].get("baths_consolidated", None),
+                "Home Type": home["description"].get("type", None),
+                "Posted": home.get("list_date", None),
+                "Home Status": home.get("status", None),
+                "Home Price": home.get("list_price", None),
+                "Home City": home["location"]["address"].get("city", None),
+                "Home Address": home["location"]["address"].get("line", None),
+                "Home Zipcode": home["location"]["address"].get("postal_code", None),
+                "Home State": home["location"]["address"].get("state", None),
+                "Lat": home["location"]["address"]["coordinate"].get("lat", None),
+                "Lon": home["location"]["address"]["coordinate"].get("lon", None),
+                "Num Beds": home["description"].get("beds", None),
+                "Num Baths": home["description"].get("baths_consolidated", None),
                 "Square Feet": home["description"].get("sqft", None),
             }
             yield home_data
-
-        # Optional: Handle pagination for additional pages
-        next_page_url = data.get("props", {}).get("pageProps", {}).get(
-            "searchPageState", {}
-        ).get("pagination", {}).get("nextUrl")
-
-        if next_page_url:
-            full_next_page_url = response.urljoin(next_page_url)
-            self.logger.info(f"Following pagination to: {full_next_page_url}")
-            yield scrapy.Request(url=full_next_page_url, callback=self.parse)
